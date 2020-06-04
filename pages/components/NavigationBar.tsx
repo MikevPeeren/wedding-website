06a748bd57ebf2e58da9539e0cce79e69eff5708
MikevPeeren <MikevPeeren@hotmail.com>
2020-06-04 20:26:37 +0200

    Added close functionality of the Menu

--- pages/components/NavigationBar.tsx
+++ pages/components/NavigationBar.tsx
@@ -5,7 +5,6 @@ import { useState } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
@@ -19,11 +18,16 @@ interface NavigationBarProps {
 }
 
 const NavigationBar = ({ title }) => {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
-  const handleMenu = (event) => {
+  console.log(anchorEl);
+
+  const handleMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   const redirectHome = (event) => {
@@ -48,25 +52,34 @@ const NavigationBar = ({ title }) => {
           }}
         >
           <MenuIcon />
-          <Menu color="inherit" id="menu-appbar" anchorEl={anchorEl} open={open}>
-            <MenuItem
-              color="inherit"
-              onClick={(event) => {
-                redirectHome(event);
-              }}
-            >
-              {HOME}
-            </MenuItem>
-            <MenuItem
-              color="inherit"
-              onClick={(event) => {
-                redirectPhotoAlbum(event);
-              }}
-            >
-              {PHOTO_ALBUM}
-            </MenuItem>
-          </Menu>
         </IconButton>
+        <Menu
+          id="menu-appbar"
+          color="inherit"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={() => {
+            handleClose();
+          }}
+        >
+          <MenuItem
+            color="inherit"
+            onClick={(event) => {
+              redirectHome(event);
+            }}
+          >
+            {HOME}
+          </MenuItem>
+          <MenuItem
+            color="inherit"
+            onClick={(event) => {
+              redirectPhotoAlbum(event);
+            }}
+          >
+            {PHOTO_ALBUM}
+          </MenuItem>
+        </Menu>
         <Typography variant="h6">{title}</Typography>
       </Toolbar>
     </AppBar>
