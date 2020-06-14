86bf9af46454586293fa7bbb50b90cf730874b44
MikevPeeren <MikevPeeren@hotmail.com>
2020-06-14 14:45:27 +0200

    Changed Header title

--- constants/general.ts
+++ constants/general.ts
@@ -1,4 +1,4 @@
-export const HEADER = 'Bruiloft';
+export const HEADER = 'Bruiloft Mike & Monika';
 export const HEADER_MIKE_AND_MONIKA = 'Mike & Monika';
 export const SUBHEADER = '25 September 2020';
 
