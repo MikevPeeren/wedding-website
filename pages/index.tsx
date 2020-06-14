0fd314527fe70737aa7e0cb78fa81be7cc3a4388
MikevPeeren <MikevPeeren@hotmail.com>
2020-06-14 15:24:06 +0200

    Changed aos offset

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,7 +55,7 @@ const Home = () => {
         <div className={styles.container__content}>
           <div>
             <img className={styles.container__dailyProgramImage} src="/static/mr&mrs.jpg"></img>
-            <div data-aos="fade-right" data-aos-offset="500" data-aos-duration="800" data-aos-easing="linear">
+            <div data-aos="fade-right" data-aos-offset="100" data-aos-duration="800" data-aos-easing="linear">
               <DailyProgram />
             </div>
           </div>
@@ -63,21 +63,21 @@ const Home = () => {
           <div className={styles.container__nightlyProgramImage}>
             <img src="/static/feesten&borrel.jpg"></img>
           </div>
-          <div data-aos="fade-left" data-aos-offset="500" data-aos-duration="800" data-aos-easing="linear">
+          <div data-aos="fade-left" data-aos-offset="100" data-aos-duration="800" data-aos-easing="linear">
             <NightlyProgram />
           </div>
 
           <div>
             <img className={styles.container__weddingLocationImage} src="/static/kloosternieuwkerk2.jpg"></img>
           </div>
-          <div data-aos="fade-right" data-aos-offset="500" data-aos-duration="800" data-aos-easing="linear">
+          <div data-aos="fade-right" data-aos-offset="100" data-aos-duration="800" data-aos-easing="linear">
             <WeddingLocation />
           </div>
 
           <div className={styles.container__nightlyProgramImage}>
             <img src="/static/butterfly.jpg"></img>
           </div>
-          <div data-aos="fade-right" data-aos-offset="500" data-aos-duration="800" data-aos-easing="linear">
+          <div data-aos="fade-right" data-aos-offset="100" data-aos-duration="800" data-aos-easing="linear">
             <ExtraInformation />
           </div>
         </div>
